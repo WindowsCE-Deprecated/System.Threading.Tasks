d597e7d4a3fa6ae0afc32fc4ecc195fbd2b8492b
Fabricio Godoy <skarllot@gmail.com>
2017-02-17 00:10:36 -0200

    New basic test for async/await

---
+++ test/Tasks.Tests/BasicAsyncTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class BasicAsyncTest
+    {
+        private static async Task<string> GetData()
+        {
+            var data = await SlowOperation();
+            return data.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private static Task<int> SlowOperation()
+        {
+            Thread.Sleep(1);
+            return Task.FromResult(7);
+        }
+
+        [TestMethod]
+        public void SimpleTest()
+        {
+            Assert.AreEqual("7", GetData().Result);
+        }
+    }
+}
