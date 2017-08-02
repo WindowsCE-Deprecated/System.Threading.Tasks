481800b7f291a93aebc53198fbd02cc10a0169d1
Fabricio Godoy <skarllot@gmail.com>
2017-08-02 02:59:05 -0300

    Update package version to 1.3.0

--- src/System.Threading.Tasks.WindowsCE/Properties/AssemblyInfo.cs
+++ src/System.Threading.Tasks.WindowsCE/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
